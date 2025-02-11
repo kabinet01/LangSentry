1543deab1bcbeadee0bbcc8031a5dc3e667c5950
TimTimTimTimTimTimTimTimTimTimTimTimTi <156430180+TimTimTimTimTimTimTimTimTimTimTimTimTi@users.noreply.github.com>
2025-02-11 23:33:25 +0800

    Create similarity.py

---
+++ semantics/similarity.py
@@ -0,0 +1,48 @@
+# comparing the two sentences using SBERT and Cosine Similarity
+
+# here's the install command
+#!pip install -U sentence-transformers
+import pandas as pd
+from sentence_transformers import SentenceTransformer, util
+
+
+# load our Sentence Transformers model pre trained!!
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+df = pd.read_csv('malignant.csv')
+
+
+# this is not production ready data!!
+sentences = [sentence.lower().replace('br','').replace('<',"").replace(">","").replace('\\',"").replace('/',"") for sentence in df.text]
+
+# lets find the semantically closest sentence to a random sentence
+# that we come up with, in our dataset
+
+
+
+# lets embed the corpus
+embeddings = model.encode(sentences)
+
+#Compute cosine similarity between my sentence, and each one in the corpus
+
+# lets go through our array and find our best one!
+# remember, we want the highest value here (highest cosine similiarity)
+while True:
+    query = input("test: ")
+    my_embedding = model.encode(query)
+
+    cos_sim = util.cos_sim(my_embedding, embeddings)
+
+    winners = []
+    for arr in cos_sim:
+        for i, each_val in enumerate(arr):
+            winners.append([sentences[i],each_val])
+
+    # lets get the top 2 sentences
+    final_winners = sorted(winners, key=lambda x: x[1], reverse=True)
+
+
+
+    for arr in final_winners[0:2]:
+        print(f'\nScore :   {arr[1]}')
+        print(f'\nSentence :   {arr[0]}')
