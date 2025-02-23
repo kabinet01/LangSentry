80178eb9fdf1928ff9a9d1b355bc9a442b6d7c2c
TimTimTimTimTimTimTimTimTimTimTimTimTi <156430180+TimTimTimTimTimTimTimTimTimTimTimTimTi@users.noreply.github.com>
2025-02-23 19:03:10 +0800

    Update __init__.py

--- python_package/langsentry/__init__.py
+++ python_package/langsentry/__init__.py
@@ -4,5 +4,6 @@ from .canary import generate_canary_token, add_canary_token, check_for_canary_le
 from .misinformation import check_misinformation
 from .check_output import load_config, extract_entities, detect_anomalies, detect_sensitive_patterns, analyze_response
 from .sanitize import sanitize_input, detect_context, detect_and_decode_invisible_unicode
+from .similarity import initialize, similarity
 
 __version__ = "0.1.0"
